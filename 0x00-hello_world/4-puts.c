2848a34b59364c7ce70a38af01b3196809ed19e6
Blaise Promise <blaepromise@gmail.com>
2022-09-08 16:19:25 -0700

    C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: always 0
+
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
