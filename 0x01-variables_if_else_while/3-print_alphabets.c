55a6cf8e9a2440d9fb5e6f7263cbbddaf15942b2
Blaise Promise <blaepromise@gmail.com>
2022-09-09 14:17:58 -0700

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,18 @@
-th
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
