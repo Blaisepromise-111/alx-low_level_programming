993ff791ae4ffa29a010c50a2b83a80f921fb790
Blaise Promise <blaepromise@gmail.com>
2022-09-13 07:37:51 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/* Program that prints _putchar and returns 0 */
+
+int main(void)
+{
+	_putchar('_')
+	_putchar('p')
+	_putchar('u')
+	_putchar('t')
+	_putchar('c')
+	_putchar('h')
+	_putchar('a')
+	_putchar('r')
+	_putchar('\n')
+	return (0)
+}
