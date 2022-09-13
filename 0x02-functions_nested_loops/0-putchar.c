681613979170415f64e5cbb55b288b80e10d7874
Blaise Promise <blaepromise@gmail.com>
2022-09-13 17:44:37 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,12 +13,11 @@ int main(void)
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-        _putchar('t');
+	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
-		
+	_putchar('\n');	
 	return (0);
 }
