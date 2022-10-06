6daa1be3463d653fb06fa19ffd21a1ae323bf93a
Blaise Promise <blaepromise@gmail.com>
2022-10-06 16:52:27 +0100

    Header files

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
+#include <stdlib.h>
+#include <stdio.h>
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
