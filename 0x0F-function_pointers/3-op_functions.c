936607086b47fde9f794da1b8dfedf44fde5dd11
Blaise Promise <blaepromise@gmail.com>
2022-10-13 04:19:09 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,73 +1,56 @@
 #include "3-calc.h"
 
 /**
- * op_add - adds two numbers
- * @a: 1st Num
- * @b: 2nd Num
- * Return: result
+ * op_add - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a + b
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - Subtracts two numbers
- * @a: 1st Num
- * @b: 2nd Num
- * Return: result
+ * op_sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a - b
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - Multiplies two numbers
- * @a: 1st Num
- * @b: 2nd Num
- * Return: result
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a * b
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - Divides  two number
- * @a: 1st Num
- * @b: 2nd Num
- * Return: result
+ * op_div - divides a number by another
+ * @a: numerator
+ * @b: denominator
+ * Return: a / b
  */
-
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a / b);
 }
 
 /**
- * op_mod - Modules of two numbers
- * @a: 1st Num
- * @b: 2nd Num
- * Return: result
+ * op_mod - returns the remainder of a division
+ * @a: numerator
+ * @b: denominator
+ * Return: a % b
  */
-
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a % b);
 }
