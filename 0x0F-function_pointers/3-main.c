7ba9e3869de51e9e392335fde0abff9af51bc296
Blaise Promise <blaepromise@gmail.com>
2022-10-13 04:18:18 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,39 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - Main ENtry
- * @argc: argument count
- * @argv: argument vector
+ * main - entry point
+ *	perform simple operations.
+ * @argc: number of commandline arguments passed
+ * @argv: an array of commandline arguments passed
  * Return: always 0
  */
-
 int main(int argc, char *argv[])
 {
+	int (*op)(int, int);
 	int a, b;
-	int (*operation)(int, int);
+	char *opstr;
 
 	if (argc != 4)
 	{
-		printf("Error\n");
+		puts("Error");
 		exit(98);
 	}
-
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	operation = get_op_func(argv[2]);
-
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-
-	printf("%d\n", operation(a, b));
-
+	opstr = argv[2];
+	op = get_op_func(opstr);
+	if (op == NULL || opstr[1] != '\0')
+	{
+		puts("Error");
+		exit(99);
+	}
+	if ((*opstr == '/' || *opstr == '%') && b == 0)
+	{
+		puts("Error");
+		exit(100);
+	}
+	printf("%d\n", op(a, b));
 	return (0);
 }
